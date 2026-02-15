00f7f70fcae8c3a5ad10bb926c285c997c84aba8
Vahid Kazemi <vkazemi@gmail.com>
2026-02-15 13:56:21 -0800

    Remove boilerplate.

--- autograd/module.py
+++ autograd/module.py
@@ -1,5 +1,5 @@
 import pickle
-from typing import Sequence
+from collections.abc import Sequence
 
 import numpy as np
 
@@ -8,9 +8,6 @@ from .tensor import Tensor
 
 
 class Module:
-    def __init__(self):
-        super().__init__()
-
     def forward(self, *args, **kwargs):
         raise NotImplementedError("Must implement forward method.")
 
@@ -32,10 +29,7 @@ class Module:
                 yield attr_name, attr
 
     def state_dict(self) -> dict:
-        state = {}
-        for name, tensor in self.named_tensors():
-            state[name] = tensor.data
-        return state
+        return {name: tensor.data for name, tensor in self.named_tensors()}
 
     def load_state_dict(self, state: dict):
         for name, tensor in self.named_tensors():
@@ -56,7 +50,6 @@ class Module:
 
 class Sequential(Module):
     def __init__(self, *modules):
-        super().__init__()
         for module in modules:
             if not isinstance(module, Module):
                 raise ValueError(f"Expected a Module, got {type(module)}")
@@ -65,10 +58,7 @@ class Sequential(Module):
     def forward(self, *args, **kwargs):
         x = args
         for module in self.modules:
-            if isinstance(x, tuple):
-                x = module(*x)
-            else:
-                x = module(x)
+            x = module(*x) if isinstance(x, tuple) else module(x)
         return x
 
     def named_tensors(self):
@@ -79,7 +69,6 @@ class Sequential(Module):
 
 class Dense(Module):
     def __init__(self, in_channels, out_channels):
-        super().__init__()
         self.weight = Tensor(kaiming([in_channels, out_channels]), requires_grad=True)
         self.bias = Tensor(np.zeros((1, out_channels)), requires_grad=True)
 
@@ -89,17 +78,8 @@ class Dense(Module):
 
 class Conv2D(Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride):
-        super().__init__()
-        if isinstance(kernel_size, Sequence):
-            if len(kernel_size) != 2:
-                raise ValueError("kernel_size should be a tuple of (height, width)")
-        else:
-            kernel_size = (kernel_size, kernel_size)
-        if isinstance(stride, Sequence):
-            if len(stride) != 2:
-                raise ValueError("stride should be a tuple of (height, width)")
-        else:
-            stride = (stride, stride)
+        kernel_size = ensure_pair(kernel_size, "kernel_size")
+        stride = ensure_pair(stride, "stride")
         self.weight = Tensor(kaiming([*kernel_size, in_channels, out_channels]), requires_grad=True)
         self.bias = Tensor(np.zeros((1, out_channels)), requires_grad=True)
         self.stride = stride
@@ -115,22 +95,13 @@ class Conv2D(Module):
 
 
 class ReLU(Module):
-    def __init__(self):
-        super().__init__()
-
     def forward(self, x):
         return x.relu()
 
 
 class AvgPool2D(Module):
     def __init__(self, kernel_size):
-        super().__init__()
-        if isinstance(kernel_size, Sequence):
-            if len(kernel_size) != 2:
-                raise ValueError("kernel_size should be a tuple of (height, width)")
-        else:
-            kernel_size = (kernel_size, kernel_size)
-        self.kernel_size = kernel_size
+        self.kernel_size = ensure_pair(kernel_size, "kernel_size")
 
     def forward(self, x):
         b, h, w, c = x.shape
@@ -140,13 +111,7 @@ class AvgPool2D(Module):
 
 class MaxPool2D(Module):
     def __init__(self, kernel_size):
-        super().__init__()
-        if isinstance(kernel_size, Sequence):
-            if len(kernel_size) != 2:
-                raise ValueError("kernel_size should be a tuple of (height, width)")
-        else:
-            kernel_size = (kernel_size, kernel_size)
-        self.kernel_size = kernel_size
+        self.kernel_size = ensure_pair(kernel_size, "kernel_size")
 
     def forward(self, x):
         b, h, w, c = x.shape
@@ -156,8 +121,15 @@ class MaxPool2D(Module):
 
 class Reshape(Module):
     def __init__(self, shape):
-        super().__init__()
         self.shape = shape
 
     def forward(self, x):
         return x.reshape(self.shape)
+
+
+def ensure_pair(value, name="value"):
+    if isinstance(value, Sequence):
+        if len(value) != 2:
+            raise ValueError(f"{name} should be a tuple of (height, width)")
+        return tuple(value)
+    return (value, value)
--- autograd/tensor.py
+++ autograd/tensor.py
@@ -19,7 +19,7 @@ class Tensor:
 
     def __str__(self):
         return str(self.data)
-    
+
     def __len__(self):
         return len(self.data)
 
@@ -33,299 +33,136 @@ class Tensor:
             grad = grad.sum(axis=tuple(reduce_axes), keepdims=True)
         return grad.reshape(self.data.shape)
 
-    def __add__(self, other):
-        if not isinstance(other, Tensor):
-            other = Tensor(other)
-        out = Tensor(
-            self.data + other.data,
-            requires_grad=self.requires_grad or other.requires_grad,
-        )
-
+    def _op(self, name, data, inputs, vjp_fn):
+        out = Tensor(data, requires_grad=any(t.requires_grad for t in inputs))
         def _backward():
-            if self.requires_grad:
-                self.grad += self._unbroadcast(out.grad)
-            if other.requires_grad:
-                other.grad += other._unbroadcast(out.grad)
-
-        out._prevs = {self, other}
+            grads = vjp_fn(out.grad)
+            for t, g in zip(inputs, grads):
+                if t.requires_grad and g is not None:
+                    t.grad += g
+        out._prevs = set(inputs)
         out._backward = _backward
-        out._name = "add"
+        out._name = name
         return out
+
+    def __add__(self, other):
+        other = ensure_tensor(other)
+        return self._op("add", self.data + other.data, [self, other],
+            lambda g: (self._unbroadcast(g), other._unbroadcast(g)))
 
     def __sub__(self, other):
-        if not isinstance(other, Tensor):
-            other = Tensor(other)
-        out = Tensor(
-            self.data - other.data,
-            requires_grad=self.requires_grad or other.requires_grad,
-        )
-
-        def _backward():
-            if self.requires_grad:
-                self.grad += self._unbroadcast(out.grad)
-            if other.requires_grad:
-                other.grad -= self._unbroadcast(out.grad)
-
-        out._prevs = {self, other}
-        out._backward = _backward
-        out._name = "subtract"
-        return out
+        other = ensure_tensor(other)
+        return self._op("sub", self.data - other.data, [self, other],
+            lambda g: (self._unbroadcast(g), other._unbroadcast(-g)))
 
     def __mul__(self, other):
-        if not isinstance(other, Tensor):
-            other = Tensor(other)
-        out = Tensor(
-            self.data * other.data,
-            requires_grad=self.requires_grad or other.requires_grad,
-        )
-
-        def _backward():
-            if self.requires_grad:
-                self.grad += self._unbroadcast(out.grad * other.data)
-            if other.requires_grad:
-                other.grad += self._unbroadcast(out.grad * self.data)
-
-        out._prevs = {self, other}
-        out._backward = _backward
-        out._name = "multiply"
-        return out
+        other = ensure_tensor(other)
+        return self._op("mul", self.data * other.data, [self, other],
+            lambda g: (self._unbroadcast(g * other.data),
+                       other._unbroadcast(g * self.data)))
 
     def __matmul__(self, other):
-        if not isinstance(other, Tensor):
-            other = Tensor(other)
-        out = Tensor(
-            self.data @ other.data,
-            requires_grad=self.requires_grad or other.requires_grad,
-        )
-
-        def _backward():
-            if self.requires_grad:
-                self.grad += out.grad @ other.data.T
-            if other.requires_grad:
-                other.grad += self.data.T @ out.grad
-
-        out._prevs = {self, other}
-        out._backward = _backward
-        out._name = "matmul"
-        return out
+        other = ensure_tensor(other)
+        return self._op("matmul", self.data @ other.data, [self, other],
+            lambda g: (g @ other.data.T, self.data.T @ g))
 
     def __truediv__(self, other):
-        if not isinstance(other, Tensor):
-            other = Tensor(other)
-        out = Tensor(
-            self.data / other.data,
-            requires_grad=self.requires_grad or other.requires_grad,
-        )
-
-        def _backward():
-            if self.requires_grad:
-                self.grad += self._unbroadcast(out.grad / other.data)
-            if other.requires_grad:
-                other.grad += self._unbroadcast(-out.grad * self.data / np.square(other.data))
-
-        out._prevs = {self, other}
-        out._backward = _backward
-        out._name = "div"
-        return out
+        other = ensure_tensor(other)
+        return self._op("div", self.data / other.data, [self, other],
+            lambda g: (self._unbroadcast(g / other.data),
+                       other._unbroadcast(-g * self.data / np.square(other.data))))
 
     def square(self):
-        out = Tensor(np.square(self.data), requires_grad=self.requires_grad)
+        return self._op("square", np.square(self.data), [self],
+            lambda g: (g * self.data * 2,))
 
-        def _backward():
-            if self.requires_grad:
-                self.grad += out.grad * self.data * 2
-
-        out._prevs = {self}
-        out._backward = _backward
-        out._name = "square"
-        return out
-    
     def log(self):
-        out = Tensor(np.log(self.data), requires_grad=self.requires_grad)
-
-        def _backward():
-            if self.requires_grad:
-                self.grad += out.grad / self.data
-
-        out._prevs = {self}
-        out._backward = _backward
-        out._name = "log"
-        return out
+        return self._op("log", np.log(self.data), [self],
+            lambda g: (g / self.data,))
 
     def exp(self):
-        out = Tensor(np.exp(self.data), requires_grad=self.requires_grad)
+        data = np.exp(self.data)
+        return self._op("exp", data, [self], lambda g: (g * data,))
 
-        def _backward():
-            if self.requires_grad:
-                self.grad += out.grad * out.data
-        
-        out._prevs = {self}
-        out._backward = _backward
-        out._name = "exp"
-        return out
+    def _softmax(self):
+        e_x = np.exp(self.data - np.max(self.data, axis=-1, keepdims=True))
+        return e_x / np.sum(e_x, axis=-1, keepdims=True)
 
     def softmax(self):
-        e_x = np.exp(self.data - np.max(self.data, axis=-1, keepdims=True))
-        out = Tensor(e_x / np.sum(e_x, axis=-1, keepdims=True), requires_grad=self.requires_grad)
-
-        def _backward():
-            if self.requires_grad:
-                s = out.data
-                self.grad += out.grad * s * (1 - s)
-                for i in range(s.shape[-1]):
-                    for j in range(s.shape[-1]):
-                        if i != j:
-                            self.grad[..., i] += out.grad[..., j] * (-s[..., i] * s[..., j])
-        
-        out._prevs = {self}
-        out._backward = _backward
-        out._name = "softmax"
-        return out
+        s = self._softmax()
+        return self._op("softmax", s, [self],
+            lambda g: (s * (g - np.sum(g * s, axis=-1, keepdims=True)),))
 
     def sparse_softmax_cross_entropy(self, target):
-        if not isinstance(target, Tensor):
-            target = Tensor(target)
-
-        e_x = np.exp(self.data - np.max(self.data, axis=-1, keepdims=True))
-        s = e_x / np.sum(e_x, axis=-1, keepdims=True)
-
-        loss = -np.log(s[np.arange(len(target)), target.data])
-        out = Tensor(loss.mean(), requires_grad=self.requires_grad)
-
-        def _backward():
-            if self.requires_grad:
-                grad = s.copy()
-                grad[np.arange(len(target)), target.data] -= 1
-                grad /= len(target)
-                self.grad += grad
-
-        out._prevs = {self}
-        out._backward = _backward
-        out._name = "sparse_softmax_cross_entropy"
-        return out
+        target = ensure_tensor(target)
+        s = self._softmax()
+        n = np.arange(len(target))
+        def vjp(g):
+            grad = s.copy()
+            grad[n, target.data] -= 1
+            grad *= g / len(target)
+            return (grad,)
+        return self._op("sparse_softmax_cross_entropy",
+            -np.log(s[n, target.data]).mean(), [self], vjp)
 
     def relu(self):
-        out = Tensor(np.maximum(self.data, 0), requires_grad=self.requires_grad)
-
-        def _backward():
-            if self.requires_grad:
-                self.grad += out.grad * (self.data > 0).astype(np.float32)
-
-        out._prevs = {self}
-        out._backward = _backward
-        out._name = "relu"
-        return out
+        return self._op("relu", np.maximum(self.data, 0), [self],
+            lambda g: (g * (self.data > 0).astype(np.float32),))
 
     def sum(self):
-        out = Tensor(np.sum(self.data), requires_grad=self.requires_grad)
-
-        def _backward():
-            if self.requires_grad:
-                self.grad += out.grad
-
-        out._prevs = {self}
-        out._backward = _backward
-        out._name = "sum"
-        return out
+        return self._op("sum", np.sum(self.data), [self], lambda g: (g,))
 
     def mean(self, axis):
-        out = Tensor(self.data.mean(axis=axis), requires_grad=self.requires_grad)
+        return self._op("mean", self.data.mean(axis=axis), [self],
+            lambda g: (np.expand_dims(g, axis=axis) / self.data.shape[axis],))
 
-        def _backward():
-            if self.requires_grad:
-                self.grad += np.ones_like(self.data) * (np.expand_dims(out.grad, axis=axis) / self.data.shape[axis])
-        
-        out._prevs = {self}
-        out._backward = _backward
-        out._name = "mean"
-        return out
+    def _extremum(self, name, axis, fn):
+        def vjp(g):
+            mask = (self.data == fn(self.data, axis=axis, keepdims=True)).astype(np.float32)
+            return (mask * (np.expand_dims(g, axis=axis) / mask.sum(axis=axis, keepdims=True)),)
+        return self._op(name, fn(self.data, axis=axis), [self], vjp)
 
     def max(self, axis):
-        out = Tensor(self.data.max(axis=axis, keepdims=False), requires_grad=self.requires_grad)
-        
-        def _backward():
-            if self.requires_grad:
-                mask = (self.data == np.max(self.data, axis=axis, keepdims=True)).astype(np.float32)
-                self.grad += mask * (np.expand_dims(out.grad, axis=axis) / mask.sum(axis=axis, keepdims=True))
-        
-        out._prevs = {self}
-        out._backward = _backward
-        out._name = "max"
-        return out
-    
+        return self._extremum("max", axis, np.max)
+
     def min(self, axis):
-        out = Tensor(self.data.max(axis=axis, keepdims=False), requires_grad=self.requires_grad)
-        
-        def _backward():
-            if self.requires_grad:
-                mask = (self.data == np.min(self.data, axis=axis, keepdims=True)).astype(np.float32)
-                self.grad += mask * (np.expand_dims(out.grad, axis=axis) / mask.sum(axis=axis, keepdims=True))
-        
-        out._prevs = {self}
-        out._backward = _backward
-        out._name = "min"
-        return out
-    
+        return self._extremum("min", axis, np.min)
+
     def argmax(self, axis=0):
-        out = Tensor(np.argmax(self.data, axis=axis), requires_grad=False)
-        out._prevs = {self}
-        out._name = "argmax"
-        return out
-    
+        return Tensor(np.argmax(self.data, axis=axis))
+
     def argmin(self, axis=0):
-        out = Tensor(np.argmin(self.data, axis=axis), requires_grad=False)
-        out._prevs = {self}
-        out._name = "argmin"
-        return out
+        return Tensor(np.argmin(self.data, axis=axis))
 
     def reshape(self, shape):
-        out = Tensor(self.data.reshape(shape), requires_grad=self.requires_grad)
-
-        def _backward():
-            if self.requires_grad:
-                self.grad += out.grad.reshape(self.shape)
-
-        out._prevs = {self}
-        out._backward = _backward
-        out._name = "reshape"
-        return out
+        return self._op("reshape", self.data.reshape(shape), [self],
+            lambda g: (g.reshape(self.shape),))
 
     def im2col(self, kernel_size, stride):
         x = self.data
         kh, kw = kernel_size
         sh, sw = stride
         b, h, w, c = x.shape
-        out_h = (h - kh) // sh + 1
-        out_w = (w - kw) // sw + 1
-
-        y = np.empty((b, out_h, out_w, kh * kw * c), dtype=self.data.dtype)
+        out_h, out_w = (h - kh) // sh + 1, (w - kw) // sw + 1
+        y = np.empty((b, out_h, out_w, kh * kw * c), dtype=x.dtype)
         for i in range(out_h):
             for j in range(out_w):
-                patch = x[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :]
-                y[:, i, j, :] = patch.reshape(b, kh * kw * c)
-        out = Tensor(y.reshape(-1, kh * kw * c), requires_grad=self.requires_grad)
-
-        def _backward():
-            if self.requires_grad:
-                out_grad = out.grad.reshape(b, out_h, out_w, kh * kw * c)
-                for i in range(out_h):
-                    for j in range(out_w):
-                        grad_patch = out_grad[:, i, j, :].reshape(b, kh, kw, c)
-                        self.grad[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :] += grad_patch
-
-        out._prevs = {self}
-        out._backward = _backward
-        out._name = "im2col"
-        return out
+                y[:, i, j, :] = x[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :].reshape(b, -1)
+        def vjp(g):
+            grad = np.zeros_like(x)
+            out_grad = g.reshape(b, out_h, out_w, kh, kw, c)
+            for i in range(out_h):
+                for j in range(out_w):
+                    grad[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :] += out_grad[:, i, j]
+            return (grad,)
+        return self._op("im2col", y.reshape(-1, kh * kw * c), [self], vjp)
 
     def backward(self):
         if not self.requires_grad:
             raise ValueError("The tensor doesn't have gradients.")
-
         ordered = []
         visited = set()
         nexts = set()
-
         def _sort(node):
             if node in nexts:
                 raise ValueError("There's a cyclic dependency.")
@@ -337,9 +174,11 @@ class Tensor:
             nexts.remove(node)
             visited.add(node)
             ordered.append(node)
-
         _sort(self)
-
         self.grad = np.ones_like(self.data)
         for tensor in reversed(ordered):
             tensor._backward()
+
+
+def ensure_tensor(x):
+    return x if isinstance(x, Tensor) else Tensor(x)
